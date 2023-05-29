fa7e63cd6cf6107d28e068504afb60fd04d72957
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 18:26:53 +0300

    feat: add page layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,63 @@
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+
+export default function Layout() {
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint='sm'
+      navbar={
+        <Navbar hidden={!opened} p='md' hiddenBreakpoint='sm' width={{ sm: 200, lg: 300 }}>
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      footer={
+        <Footer height={60} p='md'>
+          Application footer
+        </Footer>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p='md'>
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan='sm' styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size='sm'
+                color={theme.colors.gray[6]}
+                mr='xl'
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Text>
+        <Outlet></Outlet>
+      </Text>
+    </AppShell>
+  );
+}
