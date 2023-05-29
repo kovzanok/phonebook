f2b73c21e39de20181c8ad2577cf7e7d1aec1f06
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 22:14:23 +0300

    feat: add client list component

---
+++ src/components/ClientsList.tsx
@@ -0,0 +1,32 @@
+import { IClient } from "../types";
+import ClientLink from "./ClientLink";
+
+interface IClientsListProps {
+  clients: IClient[];
+}
+
+interface IListItemProps {}
+
+export default function ClientsList({ clients }: IClientsListProps) {
+  return (
+    <ul
+      style={{
+        listStyle: "none",
+        width: "70%",
+        padding: 0,
+        margin: 0,
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "10px",
+      }}
+    >
+      {clients.map(
+        (client: IClient): React.ReactElement<IListItemProps> => (
+          <li style={{ width: "100%" }} key={client.id}>
+            <ClientLink {...client} />
+          </li>
+        )
+      )}
+    </ul>
+  );
+}
