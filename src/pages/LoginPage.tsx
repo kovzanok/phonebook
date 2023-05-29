ef7368dd73e830c3c6344bb1b2d8f418a946d110
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 18:51:37 +0300

    feat: add login page layout

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import {
+  TextInput,
+  PasswordInput,
+  Button,
+  Group,
+  Box,
+  Center,
+  Text,
+  Flex,
+  MediaQuery,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
   return (
-    <div>LoginPage</div>
-  )
+    <Center h={"100vh"}>
+      <Flex gap='lg' align='center' justify='center' direction='column'>
+        <Title size='h1'>Авторизация</Title>
+        <MediaQuery styles={{ width: "300px" }} query='(min-width: 768px)'>
+          <Box
+            sx={{
+              boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.3);",
+              borderRadius: "10px",
+            }}
+            p='20px'
+            mx='auto'
+          >
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+              <TextInput
+                withAsterisk
+                label='Логин'
+                placeholder='ods'
+                {...form.getInputProps("login")}
+              />
+
+              <PasswordInput
+                withAsterisk
+                label='Пароль'
+                {...form.getInputProps("password")}
+              />
+
+              <Group position='center' mt='md'>
+                <Button type='submit'>Войти</Button>
+              </Group>
+            </form>
+          </Box>
+        </MediaQuery>
+
+        <Text>
+          Нет аккаунта? <Link to='/register'>Перейти к регистрации</Link>
+        </Text>
+      </Flex>
+    </Center>
+  );
 }
