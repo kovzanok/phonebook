dfabdfd12bedb361640696d119d1ecfa08a7e559
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-31 18:26:12 +0300

    feat: add nav bar component

---
+++ src/components/MyNavBar.tsx
@@ -0,0 +1,107 @@
+import { Navbar, Button, Flex, TextInput } from "@mantine/core";
+import ClientsList from "./ClientsList";
+import NewClientLink from "./NewClientLink";
+import { IClient } from "../types";
+
+interface IMyNavBarProps {
+  opened: boolean;
+}
+
+export default function MyNavBar({ opened }: IMyNavBarProps) {
+  const clients: IClient[] = [
+    {
+      id: 1,
+      name: "Дорорс",
+      substations: [{ id: 1, name: "Заямное", info: "ф.1" }],
+      people: [
+        {
+          position: "Энергетик",
+          name: "Матушонок",
+          phones: [{ number: "123", id: 1 }],
+          info: "",
+          id: 1,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Дорорс",
+      substations: [{ id: 1, name: "Заямное", info: "ф.1" }],
+      people: [
+        {
+          position: "Энергетик",
+          name: "Матушонок",
+          phones: [{ number: "123", id: 1 }],
+          info: "",
+          id: 1,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Дорорс",
+      substations: [{ id: 1, name: "Заямное", info: "ф.1" }],
+      people: [
+        {
+          position: "Энергетик",
+          name: "Матушонок",
+          phones: [{ number: "123", id: 1 }],
+          info: "",
+          id: 1,
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Дорорс",
+      substations: [{ id: 1, name: "Заямное", info: "ф.1" }],
+      people: [
+        {
+          position: "Энергетик",
+          name: "Матушонок",
+          phones: [{ number: "123", id: 1 }],
+          info: "",
+          id: 1,
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Дорорс",
+      substations: [{ id: 1, name: "Заямное", info: "ф.1" }],
+      people: [
+        {
+          position: "Энергетик",
+          name: "Матушонок",
+          phones: [{ number: "123", id: 1 }],
+          info: "",
+          id: 1,
+        },
+      ],
+    },
+  ];
+  return (
+    <Navbar
+      hidden={!opened}
+      p='md'
+      hiddenBreakpoint='sm'
+      width={{ sm: 300, lg: 400 }}
+    >
+      <Flex direction='column' align='center' gap='15px'>
+        <form
+          style={{
+            borderBottom: "1px solid rgb(218 221 224",
+            paddingBottom: "20px",
+          }}
+        >
+          <Flex justify='center' align='center' gap='15px'>
+            <TextInput width='30em' placeholder='Поиск'></TextInput>
+            <Button type='submit'>Поиск</Button>
+          </Flex>
+        </form>
+        <NewClientLink />
+        <ClientsList clients={clients} />
+      </Flex>
+    </Navbar>
+  );
+}
