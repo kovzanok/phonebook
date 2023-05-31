6c30476d6b96e36f0f80e7cf2d9d597f63d9a2d1
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-31 23:25:10 +0300

    feat: add substation list component

---
+++ src/components/SubstationsList/index.tsx
@@ -0,0 +1,20 @@
+import { substation } from "../../types";
+import { Title, Text } from "@mantine/core";
+import classNames from "./SubstationList.module.css";
+
+interface ISubstationListProps {
+  substations: substation[];
+}
+
+export default function SubstationList({ substations }: ISubstationListProps) {
+  return (
+    <ul className={classNames["substation-list"]}>
+      {substations.map((substation, index) => (
+        <li className={classNames["substation-item"]} key={index}>
+          <Title size='h3'>{substation.name}</Title>
+          {substation.info && <Text fz='20px'>{substation.info}</Text>}
+        </li>
+      ))}
+    </ul>
+  );
+}
