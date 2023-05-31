381a38a3f66ff0417308dbc6540a542c5a46af8e
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-31 18:25:58 +0300

    feat: add types and interfaces

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,1 +1,37 @@
+export interface IClient {
+  id: number;
+  name: string;
+  substations: [substation];
+  people?: [IPerson];
+  contacts?: [IContact];
+}
+
+interface IPerson extends IContact {
+  position: string;
+  info: string;
+}
+
+interface IContact {
+  name: string;
+  phones?: [phone];
+  destination?: [addressee];
+  id: number;
+}
+
+type substation = {
+  id: number;
+  name: string;
+  info?: string;
+};
+
+type phone = {
+  number: string;
+  id: number;
+};
+
+type addressee = {
+  text: string;
+  id: number;
+};
+
 export type handleActiveFunction = (props: { isActive: boolean }) => string;
