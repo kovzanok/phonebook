ae892a6437ba9c8b1e7179898360d7709176570e
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-01 09:35:44 +0300

    feat: add responsive styles to contacts list element

--- src/components/ContactsList/index.tsx
+++ src/components/ContactsList/index.tsx
@@ -1,8 +1,9 @@
-import { Title, Grid, Text } from "@mantine/core";
+import { Title, Text } from "@mantine/core";
 import React from "react";
 import { IContact, IPerson } from "../../types";
 import AdresseeList from "../AdresseeList";
 import classNames from "./ContactsList.module.css";
+import ContactName from "../ContactName";
 
 interface IContactsListProps {
   contacts: IPerson[] | IContact[] | undefined;
@@ -16,33 +17,18 @@ export const ContactsList = ({
     if (contacts) {
       return (
         <div className={classNames["list-wrapper"]}>
-          <Title ta='center' size='h3'>{children}</Title>
+          <Title ta='center' size='h3'>
+            {children}
+          </Title>
           <ul className={classNames["contacts-list"]}>
-            {contacts.map((contact) => {
-              return (
-                <li className={classNames["contacts-item"]}>
-                  <Grid ta='center' justify='space-between' align='center'>
-                    <Grid.Col span={3}>
-                      {"position" in contact && (
-                        <Title size='h3'>{contact.position}</Title>
-                      )}
-                      <Text fz='20px'>{contact.name}</Text>
-                    </Grid.Col>
-                    <Grid.Col span={3}>
-                      <AdresseeList list={contact.phones} />
-                    </Grid.Col>
-                    <Grid.Col span={3}>
-                      <AdresseeList list={contact.destination} />
-                    </Grid.Col>
-                    {"info" in contact && (
-                      <Grid.Col span={3}>
-                        <Text fz='20px'>{contact.info}</Text>
-                      </Grid.Col>
-                    )}
-                  </Grid>
-                </li>
-              );
-            })}
+            {contacts.map((contact) => (
+              <li className={classNames["contacts-item"]}>
+                <ContactName contact={contact}/>
+                <AdresseeList list={contact.phones} />
+                <AdresseeList list={contact.destination} />
+                {"info" in contact && <Text fz='20px'>{contact.info}</Text>}
+              </li>
+            ))}
           </ul>
         </div>
       );
