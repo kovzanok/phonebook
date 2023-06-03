8981873751c4e4843272642d824d8d028166d051
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-03 14:03:57 +0300

    feat: add logiin context

--- src/context.tsx
+++ src/context.tsx
@@ -2,3 +2,4 @@ import { createContext } from "react";
 import { authContextValueType } from "./types";
 
 export const AuthContext = createContext<authContextValueType | null>(null);
+export const LoginContext = createContext<string>("");
