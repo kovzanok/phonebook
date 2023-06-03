2cff60a87d001f0f7b89cc68af10e7b0d36842de
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-03 13:41:13 +0300

    feat: add submit login form function

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,14 +10,21 @@ import {
   MediaQuery,
   Title,
 } from "@mantine/core";
-
+import { useContext } from "react";
+import { authContextValueType } from "../types";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context";
+import { Navigate } from "react-router-dom";
+import axios from "../axios/axios";
+import Axios from "axios";
+import { useForm } from "@mantine/form";
 
 export default function LoginPage() {
   const [isAuth, setIsAuth] = useContext(AuthContext) as authContextValueType;
   if (isAuth) {
     return <Navigate to='/' />;
   }
+
   const form = useForm({
     initialValues: {
       login: "",
@@ -30,6 +37,26 @@ export default function LoginPage() {
         value.length >= 5 ? null : "Минимальная длина пароля 5 символов",
     },
   });
+
+  const submitLoginForm = async ({
+    login,
+    password,
+  }: {
+    login: string;
+    password: string;
+  }) => {
+    try {
+      const { token } = (await axios.post("/auth/login", { login, password }))
+        .data;
+      window.localStorage.setItem("phonebook-token", token);
+      setIsAuth(true);
+    } catch (error) {
+      if (Axios.isAxiosError(error)) {
+        form.setFieldError("auth", error.response?.data.message);
+      }
+    }
+  };
+
   return (
     <Center h={"100vh"}>
       <Flex gap='lg' align='center' justify='center' direction='column'>
@@ -44,6 +71,7 @@ export default function LoginPage() {
             mx='auto'
           >
             <form
+              onSubmit={form.onSubmit(submitLoginForm)}
               style={{
                 display: "flex",
                 flexDirection: "column",
@@ -61,6 +89,9 @@ export default function LoginPage() {
                 label='Пароль'
                 {...form.getInputProps("password")}
               />
+              <Text ta='center' color='red'>
+                {form.errors.auth}
+              </Text>
               <Group position='center' mt='md'>
                 <Button type='submit'>Войти</Button>
               </Group>
