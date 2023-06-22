af74bd146ac04cc7cff1ec7e0a38fed4c3d5c458
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-22 10:01:04 +0300

    feat: add actions to clients slice

--- src/store/clientsSlice.ts
+++ src/store/clientsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import myAxios from "../axios/axios";
 import { IClient } from "../types";
 import { RootState } from "./store";
@@ -20,13 +20,25 @@ const initialState: ClientsState = [];
 const clientsSlice = createSlice({
   name: "clients",
   initialState,
-  reducers: {},
+  reducers: {
+    addClient: (state, action: PayloadAction<IClient>) => {
+      state.push(action.payload);
+    },
+    updateClient: (state, action: PayloadAction<IClient>) => {
+      let clientToUpdate = state.find(
+        (client) => client._id === action.payload._id
+      );
+      clientToUpdate = action.payload;
+    },
+    removeClient: (state, action: PayloadAction<string>) =>
+      state.filter((client) => client._id !== action.payload),
+  },
   extraReducers(builder) {
     builder.addCase(fetchClients.fulfilled, (_, action) => action.payload);
   },
 });
 
-export const {} = clientsSlice.actions;
+export const { addClient, removeClient, updateClient } = clientsSlice.actions;
 
 export const clientsSelector = (state: RootState) => state.clients;
 
