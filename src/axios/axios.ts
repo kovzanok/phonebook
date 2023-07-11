8527be3b2ab60f9c3edf554cac58eafa48f288c9
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-11 11:21:27 +0300

    feat: add api url to .env file

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:3000/" });
+const instance = axios.create({ baseURL:import.meta.env.VITE_API_URL });
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("phonebook-token");
