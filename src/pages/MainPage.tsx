a4e5bdd634d6284a7ca091aa3d3b73e24c2b8fe8
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-11 10:16:47 +0300

    feat: add text when clients list is empty

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,20 @@
 import { Title } from "@mantine/core";
+import { useSelector } from "react-redux";
+import { clientsSelector } from "../store/clientsSlice";
 
 export default function Main() {
-  return <Title order={2} ta='center'>Выберите контакт</Title>;
+  const clients = useSelector(clientsSelector);
+
+  if (clients.length !== 0) {
+    return (
+      <Title mt='50px' order={2} ta='center'>
+        Выберите контакт
+      </Title>
+    );
+  }
+  return (
+    <Title mt='50px' order={2} ta='center'>
+      Контакты не найдены
+    </Title>
+  );
 }
